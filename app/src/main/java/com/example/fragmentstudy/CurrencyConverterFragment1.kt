8da337109bb2362ae4fa0091e78779baadb54264
sooyeon <s2129@e-mirim.hs.kr>
2022-07-26 09:39:39 +0900

    스피너 사용 가능하게 만들기

--- app/src/main/java/com/example/fragmentstudy/CurrencyConverterFragment1.kt
+++ app/src/main/java/com/example/fragmentstudy/CurrencyConverterFragment1.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
 
 class CurrencyConverterFragment1 : Fragment(){
@@ -34,6 +35,42 @@ class CurrencyConverterFragment1 : Fragment(){
             container,
             false //fragment 붙이기 false
         )
+
+        val calculateBtn = view.findViewById<Button>(R.id.calculate)
+        val amount = view.findViewById<EditText>(R.id.amount)
+        val result = view.findViewById<TextView>(R.id.result)
+        val fromCurrencySpinner = view.findViewById<Spinner>(R.id.from_currency)
+        val toCurrencySpinner = view.findViewById<Spinner>(R.id.to_currency)
+
+        //Spinner(드롭다운)내용 넣기
+        val currencySelectionArrayAdapter = ArrayAdapter<String>(
+            view.context,
+            android.R.layout.simple_spinner_item,
+            listOf("USD", "EUR", "JPY", "KRW")
+        )
+        
+        //android.R => 안드로이드에서 미리 만들어 놓은 디자인 가져오기
+        currencySelectionArrayAdapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_item
+        )
+
+        fromCurrencySpinner.adapter = currencySelectionArrayAdapter
+        toCurrencySpinner.adapter = currencySelectionArrayAdapter
+
+        //object 붙이면 익명클래스
+        val itemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            //계산하는 메소드 호출 형 변환 많이
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                result.text = calculateCurrency(
+                    amount.text.toString().toDouble(),
+                    fromCurrencySpinner.selectedItem.toString(),
+                    toCurrencySpinner.selectedItem.toString()
+                ).toString()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
         return view
     }
 }

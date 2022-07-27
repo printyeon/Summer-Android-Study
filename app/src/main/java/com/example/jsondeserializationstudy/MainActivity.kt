93000047d48edeefaca9e834a08f842e3f8bafdd
sooyeon <s2129@e-mirim.hs.kr>
2022-07-27 10:40:28 +0900

    json logcat에 출력해보기

---
+++ app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.jsondeserializationstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+data class MyJSONDataClass(
+    val data1: Int,
+    val data2: String,
+    val list: List<Int>)
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var mapper = jacksonObjectMapper()
+        val jsonString = """
+            {
+            "data1":1234,
+            "data2":"Hello",
+            "list":[1,2,3]
+            }
+            """.trimIndent()
+        val d1 = mapper?.readValue<MyJSONDataClass>(jsonString)
+        Log.d("mytag", d1.toString())
+    }
+}
